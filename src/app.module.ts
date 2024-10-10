2f4b0dbaa250d209ba5d4b7ef8c79d79e2864e46
Faustin Gardette <gardettefaustin@gmail.com>
2024-10-10 21:25:11 +0200

    add user routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { BoxModule } from './box/box.module';
 import { DeckModule } from './deck/deck.module';
 import { CardModule } from './card/card.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [BoxModule, DeckModule, CardModule],
+  imports: [BoxModule, DeckModule, CardModule, UserModule],
   controllers: [],
   providers: [],
 })
